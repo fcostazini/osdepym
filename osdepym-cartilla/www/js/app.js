692ac38b6cc39e34c9eb050be2f52344ce49eeaf
mauroa <mag@xamarin.com>
2015-09-17 16:57:21 -0300

    Arreglo comillas y formato

--- osdepym-cartilla/www/js/app.js
+++ osdepym-cartilla/www/js/app.js
@@ -5,37 +5,33 @@
 // the 2nd parameter is an array of 'requires'
 
 angular.module('cartilla', ['ionic', 'controllers'])
-
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
-})
-
-
-  .config(function($stateProvider, $urlRouterProvider) {
-
-  // Ionic uses AngularUI Router which uses the concept of states
-  // Learn more here: https://github.com/angular-ui/ui-router
-  // Set up the various states which the app can be in.
-  // Each state's controller can be found in controllers.js
-  $stateProvider
-
-  .state('home', {
-        url: '/home',
-        views: {
-            '': {
-                templateUrl: 'templates/home.html',
-                controller: 'HomeController',
-            }
-        }
+  .run(function($ionicPlatform) {
+    $ionicPlatform.ready(function() {
+      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+      // for form inputs)
+      if(window.cordova && window.cordova.plugins.Keyboard) {
+        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      }
+      if(window.StatusBar) {
+        StatusBar.styleDefault();
+      }
+    });
   })
-  $urlRouterProvider.otherwise('/home');
-})
+  .config(function($stateProvider, $urlRouterProvider) {
+    // Ionic uses AngularUI Router which uses the concept of states
+    // Learn more here: https://github.com/angular-ui/ui-router
+    // Set up the various states which the app can be in.
+    // Each state's controller can be found in controllers.js
+    $stateProvider
+      .state('home', {
+            url: '/home',
+            views: {
+                '': {
+                    templateUrl: 'templates/home.html',
+                    controller: 'HomeController',
+                }
+            }
+      })
+
+    $urlRouterProvider.otherwise('/home');
+  })
