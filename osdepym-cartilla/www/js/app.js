821cbdbaa02e25edbd7abcb3b7b515ac7669f199
dariocamarro <dariocamarro@gmail.com>
2015-09-17 15:22:22 -0300

    Subo el proyecto con el consumo de un servicio.

--- osdepym-cartilla/www/js/app.js
+++ osdepym-cartilla/www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+angular.module('starter', ['ionic', 'starter.controllers'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -16,4 +16,24 @@ angular.module('starter', ['ionic'])
       StatusBar.styleDefault();
     }
   });
+})
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+
+  .state('home', {
+        url: '/home',
+        views: {
+            '': {
+                templateUrl: 'templates/home.html',
+                controller: 'HomeController',
+            }
+        }
+  })
+  $urlRouterProvider.otherwise('/home');
 })
