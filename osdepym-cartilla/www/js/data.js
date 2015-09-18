0d5e1767f3b87d52b758e53ecd09273bc973f925
dariocamarro <dariocamarro@gmail.com>
2015-09-18 18:47:16 -0300

    Modifico los objetos de negocio para que se ajusten a los servicios rest del servidor. Creo un service hhtpService para probar el servicio.

--- osdepym-cartilla/www/js/data.js
+++ osdepym-cartilla/www/js/data.js
@@ -60,14 +60,15 @@ cartilla.namespace('cartilla.data.StaticDataProvider');
 
 cartilla.data.StaticDataProvider = (function() {
   var afiliados = [
-    new cartilla.model.Afiliado('31.372.955', '1531236473', 'M'),
-    new cartilla.model.Afiliado('31.489.003', '1525021015', 'M'),
-    new cartilla.model.Afiliado('32.800.512', '1540283680', 'F')
+    new cartilla.model.Afiliado('Afiliado prueba 1', '31372955', '1531236473', 'M', '20313729550', 'plata'),
+    new cartilla.model.Afiliado('Afiliado prueba 2', '31117665', '1544332112', 'M', '20311176650', 'Dorado'),
+    new cartilla.model.Afiliado('Afiliado prueba 3', '30332445', '1533231473', 'F', '20303324450', 'Bronce')
   ];
   var especialidades = [
     new cartilla.model.Especialidad('Odontología'),
     new cartilla.model.Especialidad('Pediatría'),
-    new cartilla.model.Especialidad('Traumatología')
+    new cartilla.model.Especialidad('Traumatología'),
+    new cartilla.model.Especialidad('LABORATORIO DE ANÁLISIS CLÍNIC')
   ];
   var localidades = [
     new cartilla.model.Localidad('Santos Lugares'),
@@ -79,9 +80,9 @@ cartilla.data.StaticDataProvider = (function() {
     new cartilla.model.Provincia('Corrientes')
   ];
   var prestadores = [
-    new cartilla.model.Prestador('Mauro Agnoletti', 'Traumatología', 'Chile 1333', 'Villa Raffo', 'Buenos Aires', '1675', '1531236473', ''),
-    new cartilla.model.Prestador('Facundo Costa Zini', 'Proctología', 'Las Lomas 1550', 'Villa La Cava', 'Buenos Aires', '911', '', ''),
-    new cartilla.model.Prestador('Dario Camarro', 'Ginecología', 'Belgrano 980', 'Paso de Los Libres', 'Corrientes', '1290', '', '')
+    new cartilla.model.Prestador('1','Mauro Agnoletti', 'AGUERO', 'LABORATORIO DE ANÁLISIS CLÍNIC', '1425', '-34.595140' ,'-58.409447', '555', 'Dpto. 2', 'RECOLETA', 'CAPITAL FEDERAL', ['(  54)( 011)  46431093', '(  54)( 011)  46444903'], ["Jueves de 12:00hs. a 20:00hs.","Martes de 12:00hs. a 20:00hs."]),
+    new cartilla.model.Prestador('2','Facundo Costa Zini', 'AV PTE H YRIGOYEN', 'Odontología', '1832', '-34.763066' ,'-58.403225', '9221', 'Dpto. 2', 'LOMAS DE ZAMORA', 'GBA SUR', ['(  54)( 011)  46431093', '(  54)( 011)  46444903'], ["Jueves de 12:00hs. a 20:00hs.","Martes de 12:00hs. a 20:00hs."]),
+    new cartilla.model.Prestador('3','Dario Camarro', 'AV B RIVADAVIA', 'LABORATORIO DE ANÁLISIS CLÍNIC', '1424', '-34.619247' ,'-58.438518', '5170', 'Dpto. B', 'CABALLITO', 'CAPITAL FEDERAL', ['(  54)( 011)  46431093', '(  54)( 011)  46444903'], ["Jueves de 12:00hs. a 20:00hs.","Martes de 12:00hs. a 20:00hs."])
   ];
 
   var constructor = function() { };
